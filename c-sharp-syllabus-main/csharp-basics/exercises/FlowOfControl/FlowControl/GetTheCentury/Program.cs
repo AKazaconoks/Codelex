38ae8d1503b7276ad004eaf78554713489e5c0d0
AKazaconoks <a.kazaconoks@gmail.com>
2023-02-20 17:01:33 +0200

    FlowControl done

--- c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/FlowControl/GetTheCentury/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/FlowControl/GetTheCentury/Program.cs
@@ -6,7 +6,17 @@ namespace GetTheCentury
     {
         static void Main(string[] args)
         {
+            Century(1756);
+            Century(1555);
+            Century(1000);
+            Century(1001);
+            Century(2005);
+        }
 
+        static void Century(int year)
+        {
+            if(year > 2000) Console.WriteLine("21st century");
+            else Console.WriteLine(year % 100 == 0 ? (year / 100) + "th century" : (year / 100 + 1) + "th century");
         }
     }
 }
--- c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
@@ -8,13 +8,13 @@ namespace LargestNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Input the 1st number: ");
-            var input1 = Console.ReadLine();
+            var input1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Input the 2nd number: ");
-            var input2 = Console.ReadLine();
+            var input2 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Input the 3rd number: ");
-            var input3 = Console.ReadLine();
+            var input3 = int.Parse(Console.ReadLine());
         
             /*
             todo - expected output:
@@ -22,6 +22,7 @@ namespace LargestNumber
             Input the 2nd number: 78
             Input the 3rd number: 87
              */
+            Console.WriteLine("Largest number is " + Math.Max(input1, Math.Max(input2, input3)));
         }
     }
 }
