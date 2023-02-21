89ed050eddcf5d994fbabbe32ed9ed62289896c7
AKazaconoks <a.kazaconoks@gmail.com>
2023-02-21 14:40:41 +0200

    Loops done, added ex

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Loops/Loops/Exercise9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise9
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Desired sum: ");
+            var input = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            while (true)
+            {
+                var first = random.Next(1, 7);
+                var second = random.Next(1, 7);
+                Console.WriteLine(first + " and " + second + " = " + (first + second));
+                if (first + second == input) break;
+            }
+        }
+    }
+}
---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Loops/Loops/Piglet/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Piglet
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Play();
+        }
+
+        public static void Play()
+        {
+            Console.WriteLine("Welcome to Piglet!");
+            Random random = new Random();
+            var rolled = 0;
+            var points = 0;
+            while (true)
+            {
+                rolled = random.Next(1, 7);
+                points += rolled;
+                Console.WriteLine("You rolled " + rolled);
+                if (rolled == 1)
+                {
+                    points = 0; 
+                    break;
+                }
+                Console.Write("Roll again? ");
+                if (Console.ReadLine().Contains("n")) break;
+            }
+
+            Console.WriteLine("You got " + points + " points.");
+            Console.WriteLine("Wanna play again?");
+            if (Console.ReadLine().Contains("y")) Play();
+            else Console.WriteLine("Goodbye!");
+        }
+    }
+}
