03e5cb610f21765543a57130acfb1d554c266383
AKazaconoks <a.kazaconoks@gmail.com>
2023-02-21 14:40:41 +0200

    Loops done, added ex

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Loops/Loops/Exercise11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise11
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReverseCase("Happy Birthday");
+            ReverseCase("MANY THANKS");
+            ReverseCase("sPoNtAnEoUs");
+        }
+
+        public static void ReverseCase(string input)
+        {
+            var result = "";
+            foreach (var a in input) result += Char.IsLower(a) ? Char.ToUpper(a) : Char.ToLower(a);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first word: ");
+            var first = Console.ReadLine();
+            Console.WriteLine("Enter second word: ");
+            var second = Console.ReadLine();
+            var result = first;
+            for (int i = 0; i < 30 - first.Length - second.Length; i++) result += ".";
+            Console.WriteLine(result + second);
+        }
+    }
+}
