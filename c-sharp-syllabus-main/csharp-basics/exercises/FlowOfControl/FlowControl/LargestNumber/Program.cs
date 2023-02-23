9a26264458e2b2f7926df10a8882fa05f259e8e8
AKazaconoks <a.kazaconoks@gmail.com>
2023-02-23 12:14:37 +0200

    Made branch from main

--- c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
@@ -4,7 +4,6 @@ namespace LargestNumber
 {
     class Program
     {
-        //TODO: Write a C# program to to find the largest of three numbers.
         static void Main(string[] args)
         {
             Console.WriteLine("Input the 1st number: ");
@@ -15,13 +14,7 @@ namespace LargestNumber
 
             Console.WriteLine("Input the 3rd number: ");
             var input3 = int.Parse(Console.ReadLine());
-        
-            /*
-            todo - expected output:
-            Input the 1st number: 25
-            Input the 2nd number: 78
-            Input the 3rd number: 87
-             */
+            
             Console.WriteLine("Largest number is " + Math.Max(input1, Math.Max(input2, input3)));
         }
     }
