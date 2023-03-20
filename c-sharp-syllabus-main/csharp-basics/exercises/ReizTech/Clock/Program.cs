9e965e72d41d9bc093b880c83f4e5e3b3cd5bab6
AKazaconoks <a.kazaconoks@gmail.com>
2023-03-20 09:32:21 +0200

    added missing parts

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ReizTech/Clock/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ReizTech;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var isCorrectFormat = false;
+        while (!isCorrectFormat)
+        {
+            Console.Write("Please input time in 'hh:mm' format: ");
+            var time = Console.ReadLine();
+            var parts = time.Split(":");
+            if (int.Parse(parts[0]) is >= 0 and < 24 && int.Parse(parts[1]) is >= 0 and < 60)
+            {
+                isCorrectFormat = true;
+                var hours = int.Parse(parts[0]) % 12;
+                var minutes = decimal.Parse(parts[1]) / 5;
+                Console.WriteLine($"The lesser angle between hours and minutes arrow is {Math.Min(Math.Abs(hours - minutes) * 30, 360 - Math.Abs(hours- minutes) * 30)} degrees");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect time format");
+            }  
+        }
+    }
+}
---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ReizTech/Hierarchy/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Hierarchy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var branch = new Branch();
+            branch.Branches.Add(new Branch());
+            branch.Branches[0].Branches.Add(new Branch());
+            branch.Branches.Add(new Branch());
+            branch.Branches[1].Branches.Add(new Branch());
+            branch.Branches[1].Branches[0].Branches.Add(new Branch());
+            branch.Branches[1].Branches.Add(new Branch());
+            branch.Branches[1].Branches[1].Branches.Add(new Branch());
+            branch.Branches[1].Branches[1].Branches[0].Branches.Add(new Branch());
+            branch.Branches[1].Branches[1].Branches.Add(new Branch());
+            branch.Branches[1].Branches.Add(new Branch());
+            var result = CountDepth(branch);
+            Console.WriteLine($"Tree depth is {result}");
+        }
+
+        private static int CountDepth(Branch branch)
+        {
+            if (branch.Branches.Count == 0)
+            {
+                return 1;
+            }
+
+            var maxDepth = 0;
+            foreach (var item in branch.Branches)
+            {
+                var depth = CountDepth(item);
+                maxDepth = Math.Max(depth, maxDepth);
+            }
+            return maxDepth + 1;
+        }
+    }
+}
